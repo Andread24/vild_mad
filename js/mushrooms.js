c1f2c2aaba9f14ab1459e64df9f4e513c26dca89
Agnes <agtu0001@stud.kea.dk>
2024-02-19 15:49:25 +0100

    javas

--- js/mushrooms.js
+++ js/mushrooms.js
@@ -7,7 +7,8 @@
 fetch("https://lhzhpvvlomyswblehybb.supabase.co/rest/v1/mushrooms", {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoemhwdnZsb215c3dibGVoeWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzIzOTgsImV4cCI6MjAyMzQwODM5OH0.t2Pxr9GZCPjNkKkI0Gg11I9Dyxb6VL50DMZmHE44Ma4",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoemhwdnZsb215c3dibGVoeWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzIzOTgsImV4cCI6MjAyMzQwODM5OH0.t2Pxr9GZCPjNkKkI0Gg11I9Dyxb6VL50DMZmHE44Ma4",
   },
 })
   .then((res) => res.json())
@@ -27,6 +28,11 @@ function showProduct(items) {
   copy.querySelector("img").src = items.image;
   copy.querySelector("#seasons").textContent = items.season;
   copy.querySelector("#landscape").textContent = items.landscape;
+
+  copy
+    .querySelector(".m_link")
+    .setAttribute("href", `m_singleview.html?id=${items.id}`);
+
   // appende
   document.querySelector(".grid_produktliste").appendChild(copy);
 }
---
+++ js/single_mush.js
@@ -0,0 +1,21 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const productsURL = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
+fetch(
+  "https://lhzhpvvlomyswblehybb.supabase.co/rest/v1/mushrooms",
+  {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoemhwdnZsb215c3dibGVoeWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzIzOTgsImV4cCI6MjAyMzQwODM5OH0.t2Pxr9GZCPjNkKkI0Gg11I9Dyxb6VL50DMZmHE44Ma4",
+    },
+  } + id
+)
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(items) {
+  // looper og kalder showProduct
+  items.forEach(showProduct);
+}
