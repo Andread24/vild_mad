a7ba0053607bf8abb7d748f4c62d57fb24b977d3
Andrea <andrea.dahlhansen@gmail.com>
2024-02-21 19:48:26 +0100

    cards ikke færdig

--- js/mushrooms.js
+++ js/mushrooms.js
@@ -3,6 +3,7 @@
 
 ///API nøgle:
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoemhwdnZsb215c3dibGVoeWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzIzOTgsImV4cCI6MjAyMzQwODM5OH0.t2Pxr9GZCPjNkKkI0Gg11I9Dyxb6VL50DMZmHE44Ma4
+
 fetch("https://lhzhpvvlomyswblehybb.supabase.co/rest/v1/mushrooms", {
   method: "GET",
   headers: {
@@ -18,18 +19,19 @@ function showProducts(items) {
 }
 
 function showProduct(items) {
-  const template = document.querySelector("#smallProductTemplate").content;
+  const template = document.querySelector("#ProductTemplate").content;
+  console.log(items);
   // lav en kopi
   const copy = template.cloneNode(true);
-  // ændre indholds
-  copy.querySelector("h2").textContent = items.title;
+
+  // ændre indhold
+  copy.querySelector("h4").textContent = items.title;
   copy.querySelector("img").src = items.image;
-  copy.querySelector("#seasons").textContent = items.season;
-  copy.querySelector("#landscape").textContent = items.landscape;
+  copy.querySelector(".season").textContent = items.season;
+  copy.querySelector(".landscape").textContent = items.landscape;
 
-  //ændre link så det henter id for det produkt som trykkes på
+  //ændre link, så den henter det rigtige id
   copy.querySelector("a").href = `m_singleview.html?id=${items.id}`;
-
   // appende
   document.querySelector(".grid_produktliste").appendChild(copy);
 }
